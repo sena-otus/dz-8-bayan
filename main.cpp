59ce72e29b71ca41df701528b11b9cde0a44940d
sena@s73.work <Sergey>
2024-02-19 02:52:34 +0100

    options

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 #include <iostream>
 
 
-
+namespace po = boost::program_options;
 
 const int generic_errorcode = 102;
 
@@ -20,6 +20,19 @@ int main(int, char const *[])
 {
   try
   {
+    std::vector<std::string> toscan;
+    po::options_description desc("Allowed options");
+    desc.add_options()
+      ("help,h"     , "produce help message"     )
+      ("dirs,d"     , "dirs to scan"             )
+      ("exclude,x"  , "dirs to excllude"         )
+      ("recursive,r", "scan subdirs recursively" )
+      ("minsize,m"  , "min file size (default 1)")
+      ("wildcard,w" , "wildcard for filenames"   )
+      ("blocksize,b", "blocksize (default 512)"  )
+      ("hash,h"     , "hash to use (default md5)")
+      ;
+
     std::cout << "hello" << std::endl;
   }
   catch(const std::exception &e)
